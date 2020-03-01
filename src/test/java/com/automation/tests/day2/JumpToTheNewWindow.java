a96ff6c5ce16c4dbc8bba87723945a0cd54cf250
cybertekschool <github@cybertekschool.com>
2020-03-01 16:30:51 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
@@ -4,6 +4,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Set;
+
 public class JumpToTheNewWindow {
 
     public static void main(String[] args) throws Exception{
@@ -20,6 +22,16 @@ public class JumpToTheNewWindow {
         String windowHandle = driver.getWindowHandle();
 
         System.out.println(windowHandle);
+        //getWindowHandles() - returns id's of all currently opened windows
+        //Set - doesn't allow duplicates
+
+        Set<String> windowHandles = driver.getWindowHandles();
+
+        System.out.println(windowHandles);
+
+        //since we have all windows
+        //and we know id of original window
+        //we can say switch to something that is not equals to old window id
 
 
     }
