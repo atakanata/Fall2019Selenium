be14003551b016774c60998e8c372acf7898f234
cybertekschool <github@cybertekschool.com>
2020-03-01 16:40:38 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
+++ src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
@@ -43,8 +43,21 @@ public class JumpToTheNewWindow {
 
 
         driver.close();
+    }
 
-
-
+    /**
+     *
+     * This method helps to switch in between windows based on page title
+     * @param pageTitle
+     * @param driver
+     */
+    public static void switchToWindowBasedOnTitle(String pageTitle, WebDriver driver){
+        Set<String> windows = driver.getWindowHandles();
+        for (String window: windows){
+            driver.switchTo().window(window);
+            if (driver.getTitle().equals(pageTitle)){
+                break;
+            }
+        }
     }
 }
