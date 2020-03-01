74c2bb5a671de70236f91ae4edd1f84659dc4064
cybertekschool <github@cybertekschool.com>
2020-03-01 13:53:40 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -23,7 +23,23 @@ public class BasicNavigation {
         driver.get("http://google.com");//to open a website
 
         Thread.sleep(3000);//for demo, wait 3 seconds
+        //method that return page title
+        //you can also see it as tab name, in the browser
+        String title = driver.getTitle();//returns <title>Some title</title> text
+        String expectedTitle = "Google";
 
+        System.out.println("Title is..."+title);
+
+        if(expectedTitle.equals(title)){
+            System.out.println("TEST PASSED!");
+        }else{
+            System.out.println("TEST FAILED!");
+        }
+
+
+
+        //must be at the end
         driver.close();//to close browser
+        //browser cannot close itself
     }
 }
