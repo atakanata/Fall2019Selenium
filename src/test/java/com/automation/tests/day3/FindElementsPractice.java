5ec537ade6e7f99097f8673f7be330875752d262
cybertekschool <github@cybertekschool.com>
2020-03-03 19:40:44 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -0,0 +1,13 @@
+package com.automation.tests.day3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementsPractice {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+    }
+}
