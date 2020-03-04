7509d638ca4c8423c5168a1bdea7708546d172b3
cybertekschool <github@cybertekschool.com>
2020-03-03 21:12:44 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day3/FindElementById.java
@@ -0,0 +1,37 @@
+package com.automation.tests.day3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FindElementById {
+
+    public static void main(String[] args) throws Exception{
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/login");
+        driver.findElement(By.name("username")).sendKeys("tomsmith");
+        Thread.sleep(2000);
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("SuperSecretPassword");
+
+        driver.findElement(By.id("wooden_spoon")).click();
+
+
+        Thread.sleep(2000);
+
+        String expected = "Welcome to the Secure Area. When you are done click logout below.";
+        String actual = driver.findElement(By.tagName("h4")).getText();
+
+        if (expected.equals(actual)){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+        }
+
+        driver.quit();
+    }
+}
