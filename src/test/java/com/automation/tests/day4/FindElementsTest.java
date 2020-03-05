8959354064fc6d11ea59540f7bc61b3314fb76b5
cybertekschool <github@cybertekschool.com>
2020-03-04 21:08:46 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day4/FindElementsTest.java
+++ src/test/java/com/automation/tests/day4/FindElementsTest.java
@@ -30,10 +30,12 @@ public class FindElementsTest {
 
         for(int i=1; i < links.size(); i++){
             links.get(i).click();
-            Thread.sleep(2000);
+//            Thread.sleep(2000);
 
             driver.navigate().back();
-            Thread.sleep(2000);
+//            Thread.sleep(2000);
+            //refresh list
+            links = driver.findElements(By.tagName("a"));
         }
 
 
