752a72cafdedd35f1851fc9e0436d822c9b05326
cybertekschool <github@cybertekschool.com>
2020-03-04 21:56:43 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day4/VerifyThatElementIsGone.java
+++ src/test/java/com/automation/tests/day4/VerifyThatElementIsGone.java
@@ -3,7 +3,10 @@ package com.automation.tests.day4;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class VerifyThatElementIsGone {
 
@@ -22,10 +25,24 @@ public class VerifyThatElementIsGone {
         driver.findElement(By.id("disappearing_button")).click();
         Thread.sleep(2000);
 
-        if(driver.findElements(By.id("disappearing_button")).size() == 0){
+        List<WebElement> list = driver.findElements(By.id("disappearing_button"));
+
+        //if size is 0, that means no elements were found
+        if(list.size() == 0){
             System.out.println("TEST PASSED");
         }else {
             System.out.println("TEST FAILED");
+        }
+
+        Thread.sleep(2000);
+        //to find all buttons
+        //make sure that you us findElements <--- ending is important
+        List<WebElement> buttons = driver.findElements(By.tagName("button"));
+
+        for(WebElement button: buttons){
+            //click on every button
+            button.click();
+            Thread.sleep(2000);
         }
 
         driver.quit();
