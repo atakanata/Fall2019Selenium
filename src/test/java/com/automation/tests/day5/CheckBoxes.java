938cee70d2494feb4ec8c98e017ee5ba0ad80cc8
cybertekschool <github@cybertekschool.com>
2020-03-07 13:08:39 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day5/CheckBoxes.java
@@ -0,0 +1,18 @@
+package com.automation.tests.day5;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckBoxes {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/checkboxes");
+        BrowserUtils.wait(2);
+
+
+        driver.quit();
+    }
+}
--- src/test/java/com/automation/tests/day5/RadioButtonsTest.java
+++ src/test/java/com/automation/tests/day5/RadioButtonsTest.java
@@ -5,7 +5,7 @@ import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RadioButtonsTest {
