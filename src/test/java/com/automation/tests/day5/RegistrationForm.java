9e96604802b88924fb648bbfa628e9567ea8ce97
cybertekschool <github@cybertekschool.com>
2020-03-07 17:50:33 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day5/RegistrationForm.java
+++ src/test/java/com/automation/tests/day5/RegistrationForm.java
@@ -26,15 +26,18 @@ public class RegistrationForm {
 
         List<WebElement> genders = driver.findElements(By.name("gender"));
         //select gender
-        genders.get(1).click();//select male, for example
+        genders.get(0).click();//select male, for example
 
         driver.findElement(By.name("birthday")).sendKeys("01/01/2007");
 
-        driver.findElement(By.id("inlineCheckbox2")).click();
+        driver.findElement(By.id("inlineCheckbox2")).click();//select java
 
         BrowserUtils.wait(2);
-        driver.findElement(By.id("wooden_spoon")).click();
+        driver.findElement(By.id("wooden_spoon")).click();//click on submit button
         BrowserUtils.wait(2);
+
+
+        //ADD VALIDATION PART
 
         driver.quit();
 
