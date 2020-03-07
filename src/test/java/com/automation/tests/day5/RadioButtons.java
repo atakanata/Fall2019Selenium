866e28f7d1dcc7df29aeca9899a3a79e0ab0f70e
cybertekschool <github@cybertekschool.com>
2020-03-07 11:20:53 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day5/RadioButtons.java
@@ -0,0 +1,19 @@
+package com.automation.tests.day5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RadioButtons {
+
+    public static void main(String[] args) throws Exception{
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/radio_buttons");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+        driver.quit();
+    }
+}
