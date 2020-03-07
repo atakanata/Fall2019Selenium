0a08790f30b4633147054796114e52ae3adc8db2
cybertekschool <github@cybertekschool.com>
2020-03-07 11:44:59 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day5/RadioButtons.java
+++ src/test/java/com/automation/tests/day5/RadioButtons.java
@@ -1,18 +1,42 @@
 package com.automation.tests.day5;
 
+import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class RadioButtons {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args){
         WebDriverManager.chromedriver().version("79").setup();
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://practice.cybertekschool.com/radio_buttons");
         driver.manage().window().maximize();
-        Thread.sleep(2000);
+
+        BrowserUtils.wait(2);
+        //<input type="radio">
+        List<WebElement> radioButtons = driver.findElements(By.tagName("input"));
+
+        for(WebElement radioButton: radioButtons){
+            //to check if button can be clicked
+
+            String id = radioButton.getAttribute("id");
+
+            if(radioButton.isEnabled()) {
+
+                radioButton.click();
+                System.out.println("Clicked on :: "+id);
+                BrowserUtils.wait(1);
+
+            }else {
+                System.out.println("Button is disabled, not clicked :: "+id);
+            }
+        }
 
         driver.quit();
     }
