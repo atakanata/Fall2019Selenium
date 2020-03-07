3170d57e371bda48a07afc00554b24eec71fd7f8
cybertekschool <github@cybertekschool.com>
2020-03-07 15:26:28 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day5/TestForiFrame.java
@@ -0,0 +1,20 @@
+package com.automation.tests.day5;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestForiFrame {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/iframe");
+        BrowserUtils.wait(4);
+
+
+
+        driver.quit();
+    }
+}
