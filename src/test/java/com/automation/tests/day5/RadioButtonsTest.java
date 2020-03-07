0b1081bbb01eb53f2f65f815ddc4683a519e7a5f
cybertekschool <github@cybertekschool.com>
2020-03-07 12:34:25 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day5/RadioButtonsTest.java
+++ src/test/java/com/automation/tests/day5/RadioButtonsTest.java
@@ -3,7 +3,9 @@ package com.automation.tests.day5;
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RadioButtonsTest {
@@ -16,6 +18,21 @@ public class RadioButtonsTest {
         WebDriver driver = DriverFactory.createDriver("chrome");
         driver.get("http://practice.cybertekschool.com/radio_buttons");
         BrowserUtils.wait(2);
+        //<input type="radio" id="black" name="color">
+        WebElement blackButton = driver.findElement(By.id("black"));
 
+        blackButton.click();
+
+        //how do we verify that button clicked
+        //returns true, if button clicked
+        if(blackButton.isSelected()){
+            System.out.println("TEST PASSED!");
+        }else {
+            System.out.println("TEST FAILED");
+        }
+
+
+
+        driver.quit();
     }
 }
