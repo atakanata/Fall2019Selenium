bfeb4875b41b42398d68aca759e17bb25383546d
cybertekschool <github@cybertekschool.com>
2020-03-07 13:36:03 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day5/CheckBoxesTest.java
+++ src/test/java/com/automation/tests/day5/CheckBoxesTest.java
@@ -1,8 +1,17 @@
 package com.automation.tests.day5;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CheckBoxesTest {
 
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/checkboxes");
+        //#TASK
+        //verify that 1st checkbox is not selected and 2nd is selected
 
     }
 }
