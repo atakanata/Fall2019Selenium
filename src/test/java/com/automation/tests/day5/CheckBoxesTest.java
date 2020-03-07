e7269b8f5827eb3d5179cb90315167a0b1b0f354
cybertekschool <github@cybertekschool.com>
2020-03-07 13:58:30 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day5/CheckBoxesTest.java
+++ src/test/java/com/automation/tests/day5/CheckBoxesTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.List;
 
 public class CheckBoxesTest {
