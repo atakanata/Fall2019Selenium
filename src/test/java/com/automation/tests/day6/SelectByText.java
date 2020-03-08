b22112daa639d25c1b9136432adb4198e46e6d39
cybertekschool <github@cybertekschool.com>
2020-03-08 12:31:44 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day6/SelectByText.java
+++ src/test/java/com/automation/tests/day6/SelectByText.java
@@ -46,6 +46,25 @@ public class SelectByText {
             BrowserUtils.wait(1);
 
         }
+
+        BrowserUtils.wait(5);
+
+        Select stateSelect = new Select(driver.findElement(By.id("state")));
+        stateSelect.selectByVisibleText("District Of Columbia");
+
+        //option that is currently selected
+        //getFirstSelectedOption() -- returns a webelement, that's why we need to call getText() method
+        // getText() retrieves visible text from the webelement
+        String selected = stateSelect.getFirstSelectedOption().getText();
+
+        if (selected.equals("District Of Columbia")){
+            System.out.println("TEST PASSED");
+        }else{
+            System.out.println("TEST FAILED");
+        }
+
+
+
         BrowserUtils.wait(3);
         driver.quit();
     }
