21ac0d7287da84007d3d38c05c4038013d2fc478
cybertekschool <github@cybertekschool.com>
2020-03-08 12:45:20 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
+++ src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
@@ -2,7 +2,9 @@ package com.automation.tests.day6;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class SelectByTextMultipleOptions {
 
@@ -10,9 +12,11 @@ public class SelectByTextMultipleOptions {
         WebDriver driver = DriverFactory.createDriver("chrome");
         driver.get("http://practice.cybertekschool.com/dropdown");
         BrowserUtils.wait(3);
-
-
-
+        Select languagesSelect = new Select(driver.findElement(By.name("Languages")));
+//        Whether this select element support selecting multiple options at the same time? This
+//                *         is done by checking the value of the "multiple" attribute.
+        boolean isMultiple = languagesSelect.isMultiple();
+        System.out.println(isMultiple);
 
         BrowserUtils.wait(3);
         driver.quit();
