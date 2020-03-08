93a4d26cba48acf243612f6d238cd3c1dc9779f6
cybertekschool <github@cybertekschool.com>
2020-03-08 12:54:05 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
+++ src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
@@ -4,7 +4,10 @@ import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class SelectByTextMultipleOptions {
 
@@ -21,6 +24,13 @@ public class SelectByTextMultipleOptions {
         languagesSelect.selectByVisibleText("Java");
         languagesSelect.selectByVisibleText("JavaScript");
         languagesSelect.selectByVisibleText("Python");
+
+        //let's get all selected options
+        List<WebElement> selectedLanguages = languagesSelect.getAllSelectedOptions();
+
+        for(WebElement selectedLanguage: selectedLanguages){
+            System.out.println(selectedLanguage.getText());
+        }
 
 
 
