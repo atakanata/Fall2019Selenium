e9b88679ed415bed8da5db301ebc5f23555d5ad4
cybertekschool <github@cybertekschool.com>
2020-03-08 16:43:46 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day6/Xpath.java
+++ src/test/java/com/automation/tests/day6/Xpath.java
@@ -2,7 +2,9 @@ package com.automation.tests.day6;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Xpath {
 
@@ -10,6 +12,11 @@ public class Xpath {
         WebDriver driver = DriverFactory.createDriver("chrome");
         driver.get("http://practice.cybertekschool.com/multiple_buttons");
         BrowserUtils.wait(3);
+        //value can be inside '' or ""
+        //if you don't know the tag name, or want to skip tag name, use *
+        //for example //*[@onclick='button1()']  | * means any tag name
+        WebElement btn1 = driver.findElement(By.xpath("//button[@onclick='button1()']"));
+        btn1.click();
 
 
 
