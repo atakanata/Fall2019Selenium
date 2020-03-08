96794f19d518c8892f6da6d978653acb706b280b
cybertekschool <github@cybertekschool.com>
2020-03-08 13:56:57 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day6/SelectByIndex.java
+++ src/test/java/com/automation/tests/day6/SelectByIndex.java
@@ -13,7 +13,13 @@ public class SelectByIndex {
         driver.get("http://practice.cybertekschool.com/dropdown");
         BrowserUtils.wait(3);
         Select stateSelect = new Select(driver.findElement(By.id("state")));
+        //index starts from 0
+        stateSelect.selectByIndex(9);//District Of Columbia perhaps
+        BrowserUtils.wait(3);
 
+
+        //select last option
+        stateSelect.selectByIndex(stateSelect.getOptions().size()-1);
 
 
         BrowserUtils.wait(3);
