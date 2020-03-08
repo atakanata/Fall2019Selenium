cd8bb0521bc810bcc0e28c5cf536c35cd5e771f3
cybertekschool <github@cybertekschool.com>
2020-03-08 12:41:08 -0400

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day6/SelectByTextMultipleOptions.java
@@ -0,0 +1,20 @@
+package com.automation.tests.day6;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.WebDriver;
+
+public class SelectByTextMultipleOptions {
+
+    public static void main(String[] args) {
+        WebDriver driver = DriverFactory.createDriver("chrome");
+        driver.get("http://practice.cybertekschool.com/dropdown");
+        BrowserUtils.wait(3);
+
+
+
+
+        BrowserUtils.wait(3);
+        driver.quit();
+    }
+}
