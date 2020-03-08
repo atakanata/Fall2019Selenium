a738a03f81f4a3860e2e94e87fb36655c5466f33
cybertekschool <github@cybertekschool.com>
2020-03-08 13:36:58 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day6/NoSelectDropdown.java
+++ src/test/java/com/automation/tests/day6/NoSelectDropdown.java
@@ -4,6 +4,9 @@ import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class NoSelectDropdown {
 
@@ -15,8 +18,18 @@ public class NoSelectDropdown {
         driver.findElement(By.id("dropdownMenuLink")).click();//to expand dropdown
 
         //<a class="dropdown-item" href="https://www.amazon.com/">Amazon</a>
-        BrowserUtils.wait(2);
-        driver.findElement(By.linkText("Amazon")).click();//click on option
+        BrowserUtils.wait(3);
+
+        List<WebElement> allLinks = driver.findElements(By.className("dropdown-item"));
+        for (WebElement link: allLinks){
+            System.out.println(link.getText()+" : "+link.getAttribute("href"));
+        }
+
+
+
+
+
+        driver.findElement(By.linkText("Etsy")).click();//click on option
 
         BrowserUtils.wait(3);
         driver.quit();
