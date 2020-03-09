081857d00fd06c9756c96f8ea4beef40e3205d86
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-03-09 16:15:54 -0400

    updated DriverFactory class

--- src/test/java/com/automation/utilities/DriverFactory.java
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -8,13 +8,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
-    /**
-     * You homework will be to improve this method!
-     * This method return webdriver object based on browser type
-     * If you want to use chrome browser, just provide chrome as a parameter
-     * @param browserName
-     * @return webdriver object
-     */
+
     public static WebDriver createDriver(String browserName){
         if(browserName.equalsIgnoreCase("chrome")){
             //to fix [1583364253.062][SEVERE]: Timed out receiving message from renderer: 0.100
