b1bd885ac0ff46c0dcd01f1c4bb3c405b0d383f0
cybertekschool <github@cybertekschool.com>
2020-03-10 20:29:16 -0400

    day 7: locators

--- src/test/java/com/automation/tests/day7/Xpath.java
+++ src/test/java/com/automation/tests/day7/Xpath.java
@@ -2,15 +2,17 @@ package com.automation.tests.day7;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Xpath {
+    static String userNameLocator = "//label[text()='Username']/following-sibling::input";
 
     public static void main(String[] args) {
         WebDriver driver = DriverFactory.createDriver("chrome");
         driver.get("http://practice.cybertekschool.com/login");
         BrowserUtils.wait(3);
-
+        driver.findElement(By.xpath(userNameLocator));
 
 
         BrowserUtils.wait(3);
