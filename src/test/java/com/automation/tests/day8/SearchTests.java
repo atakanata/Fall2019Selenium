0aab13a403644f7c31c290bcab213267fa957697
cybertekschool <github@cybertekschool.com>
2020-03-14 13:42:37 -0400

    day 8

--- src/test/java/com/automation/tests/day8/SearchTests.java
+++ src/test/java/com/automation/tests/day8/SearchTests.java
@@ -1,4 +1,29 @@
 package com.automation.tests.day8;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class SearchTests {
+    private WebDriver driver;
+
+
+
+
+
+    @BeforeMethod
+    public void setup(){
+        //setup webdriver
+        WebDriverManager.chromedriver().version("79").setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void teardown(){
+        //close browser and destroy webdriver object
+        driver.quit();
+    }
+
 }
