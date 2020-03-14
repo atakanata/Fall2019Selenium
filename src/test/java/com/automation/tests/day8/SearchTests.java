18fd8258c928832614140943c9ba21158893754f
cybertekschool <github@cybertekschool.com>
2020-03-14 13:56:10 -0400

    day 8

--- src/test/java/com/automation/tests/day8/SearchTests.java
+++ src/test/java/com/automation/tests/day8/SearchTests.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,7 +26,13 @@ public class SearchTests {
         //it's the easiest way to collect all of them
         List<WebElement> searchItems = driver.findElements(By.tagName("h3"));
         for(WebElement searchItem: searchItems){
-            System.out.println(searchItem.getText());
+            String var = searchItem.getText();
+            //if there is a text - print it
+            if(!var.isEmpty()){
+                System.out.println(var);
+                //verify that every search result contains java
+                Assert.assertTrue(var.toLowerCase().contains("java"));
+            }
         }
     }
 
