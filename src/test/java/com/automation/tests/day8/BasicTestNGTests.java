f61b3f47277c4b44acefe5294b8f8eeed4906af1
cybertekschool <github@cybertekschool.com>
2020-03-14 12:44:35 -0400

    day 8

--- src/test/java/com/automation/tests/day8/BasicTestNGTests.java
+++ src/test/java/com/automation/tests/day8/BasicTestNGTests.java
@@ -1,4 +1,12 @@
 package com.automation.tests.day8;
 
+import org.testng.Assert;
+
 public class BasicTestNGTests {
+
+    public void test1(){
+        String expected = "apple";
+        String actual = "apple";
+        Assert.assertEquals(actual,expected);
+    }
 }
