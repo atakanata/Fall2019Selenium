92d47bd3180b2b68372f65408eb23197f59a8b41
cybertekschool <github@cybertekschool.com>
2020-03-14 13:26:03 -0400

    day 8

--- src/test/java/com/automation/tests/day8/BasicTestNGTests.java
+++ src/test/java/com/automation/tests/day8/BasicTestNGTests.java
@@ -1,11 +1,23 @@
 package com.automation.tests.day8;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BasicTestNGTests {
+    //Runs only once in the class before @beforemethod and before any test
+    //regardless on number of tests, it runs only once
+    @BeforeClass
+    public void beforeClass(){
+        //something that should be done only once in the class before all tests
+        System.out.println("BEFORE CLASS");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        //something that should be done only once in the class after all tests
+        System.out.println("AFTER CLASS");
+    }
+
     //runs before every test automatically
     //works as a pre-condition or setup
     @BeforeMethod
