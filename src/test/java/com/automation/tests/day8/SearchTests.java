7309477605c59d3435bde5d14b7f7e6e7ead6706
cybertekschool <github@cybertekschool.com>
2020-03-14 13:50:32 -0400

    day 8

--- src/test/java/com/automation/tests/day8/SearchTests.java
+++ src/test/java/com/automation/tests/day8/SearchTests.java
@@ -1,17 +1,33 @@
 package com.automation.tests.day8;
 
+import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class SearchTests {
     private WebDriver driver;
 
-
-
-
+    @Test
+    public void googleSearchTest(){
+        driver.get("http://google.com");
+        driver.findElement(By.name("q")).sendKeys("java", Keys.ENTER);
+        BrowserUtils.wait(2);
+        //since every search item has a tag name <h3>
+        //it's the easiest way to collect all of them
+        List<WebElement> searchItems = driver.findElements(By.tagName("h3"));
+        for(WebElement searchItem: searchItems){
+            System.out.println(searchItem.getText());
+        }
+    }
 
     @BeforeMethod
     public void setup(){
