14a735cf76029bc397a50728eaf5d4d0366c3faf
cybertekschool <github@cybertekschool.com>
2020-03-14 12:51:07 -0400

    day 8

--- src/test/java/com/automation/tests/day8/BasicTestNGTests.java
+++ src/test/java/com/automation/tests/day8/BasicTestNGTests.java
@@ -1,12 +1,23 @@
 package com.automation.tests.day8;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class BasicTestNGTests {
 
-    public void test1(){
+    @Test
+    public void test1() {
         String expected = "apple";
         String actual = "apple";
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void test2() {
+        int num1 = 5;
+        int num2 = 10;
+        //it calls hard assertion.
+        //if assertion fails - it stops the execution (due to exception).
+        Assert.assertTrue(num1 > num2);
     }
 }
