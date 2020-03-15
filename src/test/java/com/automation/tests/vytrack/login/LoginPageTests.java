067e02835f2f3f88a799a1e2bea0a91d7e1408d6
cybertekschool <github@cybertekschool.com>
2020-03-15 12:14:28 -0400

    day 8

--- src/test/java/com/automation/tests/vytrack/login/LoginPageTests.java
+++ src/test/java/com/automation/tests/vytrack/login/LoginPageTests.java
@@ -3,15 +3,30 @@ package com.automation.tests.vytrack.login;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+//STATIC IMPORT OF ALL ASSERTIONS
+import static org.testng.Assert.*;
 
 public class LoginPageTests {
     private WebDriver driver;
 
     @BeforeMethod
-    public void setup(){
+    public void setup() {
         WebDriverManager.chromedriver().version("79").setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        //if webdriver object alive
+        if (driver != null) {
+            //close browser, close session
+            driver.quit();
+            //destroy webdriver object for sure
+            driver = null;
+        }
     }
 }
