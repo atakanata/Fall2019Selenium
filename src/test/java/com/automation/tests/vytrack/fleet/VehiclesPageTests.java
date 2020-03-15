23bc246d97627c61883d7d59320c467139f51f53
cybertekschool <github@cybertekschool.com>
2020-03-15 13:58:27 -0400

    day 9 vytrack automation

---
+++ src/test/java/com/automation/tests/vytrack/fleet/VehiclesPageTests.java
@@ -0,0 +1,80 @@
+package com.automation.tests.vytrack.fleet;
+
+import com.automation.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ ######## TASK
+
+ under fleet package create a class called VehiclesPageTests
+
+ In this class, you will need to create @beforemethod with setup and @aftermethod with teardown part. Use LoginPageTests class as a reference.
+
+ create a test called verifyPageSubTitle
+ - in this test, you will need to navigate to Fleet --> Vehicles and verify that page subtitle is equals to "All Cars"
+
+ user assertions for validation.
+ */
+public class VehiclesPageTests {
+    private String URL = "https://qa2.vytrack.com/user/login";
+    //    CREDENTIALS FOR store manager
+    private String username = "storemanager85";
+    private String password = "UserUser123";
+
+    private By usernameBy = By.id("prependedInput");
+    private By passwordBy = By.id("prependedInput2");
+
+    private By fleetBy = By.xpath("//span[@class='title title-level-1' and contains(text(),'Fleet')]");
+    private By subtitleBy = By.className("oro-subtitle");
+    private WebDriver driver;
+
+    @Test
+    public void verifyPageSubTitle(){
+        driver.findElement(usernameBy).sendKeys(username);
+        driver.findElement(passwordBy).sendKeys(password, Keys.ENTER);
+
+        BrowserUtils.wait(5);
+        //click on fleet
+        driver.findElement(fleetBy).click();
+
+        BrowserUtils.wait(2);
+
+        //click on Vehicles
+        driver.findElement(By.linkText("Vehicles")).click();
+
+        BrowserUtils.wait(5);
+
+        WebElement subTitleElement = driver.findElement(subtitleBy);
+        System.out.println(subTitleElement.getText());
+
+    }
+
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().version("79").setup();
+        driver = new ChromeDriver();
+        driver.get(URL);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        //if webdriver object alive
+        if (driver != null) {
+            //close browser, close session
+            driver.quit();
+            //destroy webdriver object for sure
+            driver = null;
+        }
+    }
+}
