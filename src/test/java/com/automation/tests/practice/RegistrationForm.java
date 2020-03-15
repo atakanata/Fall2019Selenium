df452531ade639a86234839dd354cc26b9fec641
cybertekschool <github@cybertekschool.com>
2020-03-15 17:00:22 -0400

    day 9 vytrack automation

--- src/test/java/com/automation/tests/practice/RegistrationForm.java
+++ src/test/java/com/automation/tests/practice/RegistrationForm.java
@@ -1,15 +1,46 @@
 package com.automation.tests.practice;
 
+import com.sun.tools.javac.util.StringUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.jsoup.helper.StringUtil;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class RegistrationForm {
 
     private String URL = "http://practice.cybertekschool.com/registration_form";
     private WebDriver driver;
+    // p tag name of success message
+    // one xpath for all inputs: //label[text()='Label name']/..//input
+    private By firstNameBy = By.name("firstname");
+    private By lastNameBy = By.name("lastname");
+    private By usernameBy = By.name("username");
+    private By emailBy = By.name("email");
+    private By passwordBy = By.name("password");
+    private By phoneBy = By.name("phone");
+    //gender
+    private By maleBy = By.cssSelector("input[value='male']");
+    private By femaleBy = By.cssSelector("input[value='female']");
+    private By otherBy = By.cssSelector("input[value='other']");
+
+    private By dateOfBirthBy = By.name("birthday");
+    private By departmentBy = By.name("department");
+    private By jobTitleBy = By.name("job_title");
+
+    //languages
+    private By cplusplusBy = By.xpath("//label[text()='C++']/preceding-sibling::input");
+    private By javaBy = By.xpath("//label[text()='Java']/preceding-sibling::input");
+    private By javascriptBy = By.xpath("//label[text()='JavaScript']/preceding-sibling::input");
+
+    @Test
+    public void test1(){
+
+    }
+
 
     @BeforeMethod
     public void setup(){
