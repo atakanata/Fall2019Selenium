3b375eebf7ac84f7da63f7d6f11c0f50e8f1f72c
cybertekschool <github@cybertekschool.com>
2020-03-15 16:26:42 -0400

    day 9 vytrack automation

---
+++ src/test/java/com/automation/tests/practice/RegistrationForm.java
@@ -0,0 +1,27 @@
+package com.automation.tests.practice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class RegistrationForm {
+
+    private String URL = "http://practice.cybertekschool.com/registration_form";
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        WebDriverManager.chromedriver().version("79").setup();
+        driver = new ChromeDriver();
+        driver.get(URL);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void teardown(){
+        driver.quit();
+    }
+
+}
