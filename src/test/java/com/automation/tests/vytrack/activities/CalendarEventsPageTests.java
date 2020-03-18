04e1d89a63af958e4eeeb5a1709a4004bb35025b
cybertekschool <github@cybertekschool.com>
2020-03-17 20:07:57 -0400

    day 9 vytrack automation

---
+++ src/test/java/com/automation/tests/vytrack/activities/CalendarEventsPageTests.java
@@ -0,0 +1,13 @@
+package com.automation.tests.vytrack.activities;
+
+/**
+ * TASK until 8:14
+ *
+ * Scenario: Verify for store manager
+ *
+ * Login as story manager
+ * Go to Activities --> Calendar Events
+ * Verify that Create Calendar Event button is displayed
+ */
+public class CalendarEventsPageTests {
+}
