15f8d7a7bacfbac1e81399e1039d39599a284d88
cybertekschool <github@cybertekschool.com>
2020-03-18 19:34:06 -0400

    day 10 Actions

--- src/test/java/com/automation/tests/day10/ActionsTests.java
+++ src/test/java/com/automation/tests/day10/ActionsTests.java
@@ -2,27 +2,43 @@ package com.automation.tests.day10;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ActionsTests {
     private WebDriver driver;
+    private Actions actions;
 
     @BeforeMethod
-    public void setup(){
-       driver = DriverFactory.createDriver("chrome");
+    public void setup() {
+        driver = DriverFactory.createDriver("chrome");
+        actions = new Actions(driver);
     }
 
     @Test
-    public void hoverOnImage(){
+    public void hoverOnImage() {
         driver.get("http://practice.cybertekschool.com/hovers");
         BrowserUtils.wait(3);
+
+        WebElement img1 = driver.findElement(By.xpath("(//img)[1]"));
+        WebElement img2 = driver.findElement(By.xpath("(//img)[2]"));
+
+        //build() is needed when you have couple of actions
+        //always end with perform()
+        actions.moveToElement(img1).
+                pause(1000).
+                moveToElement(img2).
+                build().perform();
     }
 
     @AfterMethod
-    public void teardown(){
+    public void teardown() {
+        BrowserUtils.wait(3);
         driver.quit();
     }
 }
