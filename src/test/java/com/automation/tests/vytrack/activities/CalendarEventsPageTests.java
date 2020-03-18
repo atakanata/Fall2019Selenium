10680f6eaeb8a71c390ce03c9aa6773527ed8d0f
Atakan <atakanata@gmail.com>
2020-03-18 01:07:11 -0400

    all files

---
+++ src/test/java/com/automation/tests/vytrack/activities/CalendarEventsPageTests.java
@@ -0,0 +1,108 @@
+package com.automation.tests.vytrack.activities;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * TASK until 8:14
+ *
+ * Scenario: Verify for store manager
+ *
+ * Login as story manager
+ * Go to Activities --> Calendar Events
+ * Verify that Create Calendar Event button is displayed
+ */
+public class CalendarEventsPageTests {
+
+    private WebDriver driver;
+    private By usernameBy = By.id("prependedInput");
+    private By passwordBy = By.id("prependedInput2");
+    private Actions actions;
+
+    private String storeManagerUserName="storemanager85";
+    private String storeManagerPassword="UserUser123";
+    private By activitiesBy = By.xpath("//span[@class='title title-level-1' and contains(text(),'Activities')]");
+    private By logCallBtnBy = By.cssSelector("a[title='Log call']");
+    private By currentuserBy = By.cssSelector("#user-menu > a");
+    private By ownerBy = By.className("select2-chosen");
+    private By titleBy = By.cssSelector("[name='oro_calendar_event_form[title]']");
+    private By startDateBy = By.cssSelector("[id*='date_selector_oro_calendar_event_form_start-uid']");
+    private By startTimeBy = By.cssSelector("[id*='time_selector_oro_calendar_event_form_start-uid']");
+
+    @BeforeMethod
+    public void setup(){
+
+        driver = DriverFactory.createDriver("chrome");
+        driver.get("https://qa2.vytrack.com/user/login");
+        driver.manage().window().maximize();
+
+        actions = new Actions(driver);
+
+        BrowserUtils.wait(3);
+
+        driver.findElement(usernameBy).sendKeys(storeManagerUserName);
+        driver.findElement(passwordBy).sendKeys(storeManagerPassword, Keys.ENTER);
+
+        BrowserUtils.wait(5);
+
+        //hover over Activities
+        actions.moveToElement(driver.findElement(activitiesBy)).perform();
+
+        BrowserUtils.wait(2);
+        driver.findElement(By.linkText("Calendar Events")).click();
+
+        BrowserUtils.wait(5);
+    }
+
+    @AfterMethod
+    public void teardown(){
+        driver.quit();
+    }
+
+    @Test
+    public void verifyCreateEventButton(){
+        WebElement createEvent = driver.findElement(By.cssSelector("a[title='Create Calendar event']"));
+        Assert.assertTrue(createEvent.isDisplayed());
+    }
+
+    @Test
+    public void verifyDefaultOptions(){
+        driver.findElement(By.cssSelector("a[title='Create Calendar event']")).click();
+        BrowserUtils.wait(3);
+        String currentUserName = driver.findElement(currentuserBy).getText().trim();
+        String defaultOwnerNAme = driver.findElement(ownerBy).getText().trim();
+        Assert.assertEquals(currentUserName, defaultOwnerNAme);
+
+        //        Default title should be blank
+        WebElement titleElement = driver.findElement(titleBy);
+        Assert.assertTrue(titleElement.getAttribute("value").isEmpty());
+
+        //date time syntax = https://www.journaldev.com/17899/java-simpledateformat-java-date-format
+
+        //Default start date should be current date
+        String expectedDate = LocalDate.now().format(DateTimeFormatter.ofPattern("MMM dd, yyyy"));
+        String actualDate = driver.findElement(startDateBy).getAttribute("value");
+
+        Assert.assertEquals(actualDate , expectedDate);
+
+        String expectedTime = LocalTime.now(ZoneId.of("GMT-7")).format(DateTimeFormatter.ofPattern("h:m a"));
+        String actualTime = driver.findElement(startTimeBy).getAttribute("value");
+
+        Assert.assertEquals(actualTime, expectedTime);
+
+    }
+}
