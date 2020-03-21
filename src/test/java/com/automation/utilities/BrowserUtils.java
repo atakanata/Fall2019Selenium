5c43525dd76c575af2c9dfee3bf6f2ee5376de81
atakanata <atakanata@gmail.com>
2020-03-21 17:56:36 -0400

    3-21

--- src/test/java/com/automation/utilities/BrowserUtils.java
+++ src/test/java/com/automation/utilities/BrowserUtils.java
@@ -1,5 +1,10 @@
 package com.automation.utilities;
 
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrowserUtils {
 
     /**
@@ -12,5 +17,14 @@ public class BrowserUtils {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public static List<String> getTextFromWebElements(List<WebElement> elements){
+        List<String> textValues = new ArrayList<>();
+        for (WebElement element:elements
+             ) {
+            textValues.add(element.getText());
+        }
+        return textValues;
     }
 }
