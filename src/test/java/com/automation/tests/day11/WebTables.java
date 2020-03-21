736928806f3586a2dccaf926996688f9f288d458
cybertekschool <github@cybertekschool.com>
2020-03-21 16:22:01 -0400

    day 11 web tables

--- src/test/java/com/automation/tests/day11/WebTables.java
+++ src/test/java/com/automation/tests/day11/WebTables.java
@@ -2,9 +2,12 @@ package com.automation.tests.day11;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -18,7 +21,14 @@ public class WebTables {
 
     @BeforeMethod
     public void setup() {
-        driver = DriverFactory.createDriver("chrome");
+        WebDriverManager.chromedriver().version("79").setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        //headless mode makes execution twice faster
+        //it does everything except file uploading
+        //set it to tru to make it work
+        chromeOptions.setHeadless(false);//to run browser without GUI. Makes browser invisible.
+        driver = new ChromeDriver(chromeOptions);
+
         driver.get("http://practice.cybertekschool.com/tables");
         driver.manage().window().maximize();
         BrowserUtils.wait(3);
