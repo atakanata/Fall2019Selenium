9819cf1aaf22bd6e3633203915b4a6c2bca1ede8
cybertekschool <github@cybertekschool.com>
2020-03-21 15:45:39 -0400

    day 11 web tables

--- src/test/java/com/automation/tests/day11/WebTables.java
+++ src/test/java/com/automation/tests/day11/WebTables.java
@@ -5,6 +5,7 @@ import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -35,9 +36,12 @@ public class WebTables {
         List<String> expected = Arrays.asList("Last Name", "First Name", "Email", "Due", "Web Site", "Action");
         List<WebElement> columnNames = driver.findElements(By.xpath("//table[1]//th"));
 
+        //just to print
         for (WebElement columnName : columnNames) {
             System.out.println(columnName.getText());
         }
+
+        Assert.assertEquals(BrowserUtils.getTextFromWebElements(columnNames), expected);
     }
 
 }
