5c1a0f63f8a0aa595a64140d2321b7071d958144
cybertekschool <github@cybertekschool.com>
2020-03-21 15:32:50 -0400

    day 11 web tables

--- src/test/java/com/automation/tests/day11/WebTables.java
+++ src/test/java/com/automation/tests/day11/WebTables.java
@@ -2,10 +2,14 @@ package com.automation.tests.day11;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class WebTables {
     private WebDriver driver;
@@ -25,8 +29,12 @@ public class WebTables {
     }
 
     @Test
-    public void getColumnNames(){
-
+    public void getColumnNames() {
+        //th - represents table header cells
+        List<WebElement> columnNames = driver.findElements(By.xpath("//table[1]//th"));
+        for (WebElement columnName : columnNames) {
+            System.out.println(columnName.getText());
+        }
     }
 
 }
