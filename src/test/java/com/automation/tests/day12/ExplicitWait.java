79bff152be6d988c1200ba1b75fc00b53b8f5b71
cybertekschool <github@cybertekschool.com>
2020-03-22 15:14:27 -0400

    day 12 warm-up

--- src/test/java/com/automation/tests/day12/ExplicitWait.java
+++ src/test/java/com/automation/tests/day12/ExplicitWait.java
@@ -1,7 +1,9 @@
 package com.automation.tests.day12;
 
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -25,9 +27,29 @@ public class ExplicitWait {
         //wait up to 10 seconds until title contains Google
         wait.until(ExpectedConditions.titleIs("Google"));
 
-
         driver.navigate().to("https://amazon.com");
         wait.until(ExpectedConditions.titleContains("Amazon"));
+
+    }
+
+    @Test
+    public void waitForVisibility(){
+        driver.get("http://practice.cybertekschool.com/dynamic_loading/1");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        driver.findElement(By.tagName("button")).click();
+
+        WebElement username = driver.findElement(By.name("username"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        wait.until(ExpectedConditions.visibilityOf(username)).sendKeys("tomsmith");
+        wait.until(ExpectedConditions.visibilityOf(password)).sendKeys("SuperSecretPassword");
+
+        wait.until(ExpectedConditions.visibilityOf(submitBtn));
+        wait.until(ExpectedConditions.elementToBeClickable(submitBtn)).click();
+
 
     }
 
