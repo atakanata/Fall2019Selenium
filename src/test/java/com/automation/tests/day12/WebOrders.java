5bbb5637fd797fe301753cf57d963d1101b261dd
cybertekschool <github@cybertekschool.com>
2020-03-22 17:11:14 -0400

    day 12 implicit, explicit and fluent waits

---
+++ src/test/java/com/automation/tests/day12/WebOrders.java
@@ -0,0 +1,20 @@
+package com.automation.tests.day12;
+
+import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class WebOrders {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup(){
+        driver = DriverFactory.createDriver("chrome");
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/weborders");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.xpath("ctl00_MainContent_password")).sendKeys("test", Keys.ENTER);
+    }
+}
