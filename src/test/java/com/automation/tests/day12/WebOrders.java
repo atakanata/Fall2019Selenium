57dc7c225ee923bc3bf08c6b287c10d0f82140bb
cybertekschool <github@cybertekschool.com>
2020-03-22 17:23:30 -0400

    day 12 implicit, explicit and fluent waits

--- src/test/java/com/automation/tests/day12/WebOrders.java
+++ src/test/java/com/automation/tests/day12/WebOrders.java
@@ -4,17 +4,46 @@ import com.automation.utilities.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class WebOrders {
 
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @BeforeMethod
-    public void setup(){
+    public void setup() {
         driver = DriverFactory.createDriver("chrome");
+        wait = new WebDriverWait(driver, 10);
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/weborders");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test", Keys.ENTER);
+    }
+
+    /**
+     * 7 minutes until 5:21
+     * Go to web orders page
+     * Click on "Check All" button
+     * Verify that all records are selected
+     */
+    @Test
+    public void checkBoxTest() {
+        driver.findElement(By.id("ctl00_MainContent_btnCheckAll")).click();
+        List<WebElement> checkboxes = driver.findElements(By.cssSelector("input[type='checkbox']"));
+        for (WebElement checkbox : checkboxes) {
+            Assert.assertTrue(checkbox.isSelected());
+        }
+    }
+
+    @AfterMethod
+    public void teardown() {
+        driver.quit();
     }
 }
