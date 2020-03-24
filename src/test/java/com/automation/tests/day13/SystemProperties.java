a427213537c55861069df4725730b9f4c7590a50
cybertekschool <github@cybertekschool.com>
2020-03-24 19:42:10 -0400

    day 13 System properties

--- src/test/java/com/automation/tests/day13/SystemProperties.java
+++ src/test/java/com/automation/tests/day13/SystemProperties.java
@@ -7,6 +7,20 @@ public class SystemProperties {
 
     @Test
     public void test(){
+        // /Users/studio2/IdeaProjects/Fall2019Selenium/pom.xml
+        // System.getProperty("user.dir") + "/pom.xml"
         System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("os.name"));
+        //flexible path to downloads folder
+        //System.getProperty("user.home")+"/Downloads"
+        System.out.println(System.getProperty("user.home"));
+        //for windows, use \\ instead of /
+        String pathToDownloads = System.getProperty("user.home")+"/Downloads";
+
+        System.out.println(pathToDownloads);
+
+        System.out.println(System.getProperty("os.arch"));
+
+        //https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html
     }
 }
