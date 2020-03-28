9a9ad1a69da139844931ea895dd6ce60a08a068e
cybertekschool <github@cybertekschool.com>
2020-03-28 12:48:55 -0400

    day 14 page object model

---
+++ src/test/java/com/automation/pages/LoginPage.java
@@ -0,0 +1,25 @@
+package com.automation.pages;
+
+import com.automation.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(id = "prependedInput")
+    public WebElement username;
+
+    @FindBy(id = "prependedInput2")
+    public WebElement password;
+
+    @FindBy(id = "_submit")
+    public WebElement login;
+
+    public LoginPage() {
+        //to connect our webdriver, page class and page factory
+        //PageFactory - used to use @FindBy annotations
+        //PageFactory - helps to find elements easier
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+}
