9f05f2c4e1c97b12cc23407f76587ed2039a2bfc
cybertekschool <github@cybertekschool.com>
2020-03-28 13:27:33 -0400

    day 14 page object model

---
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -0,0 +1,17 @@
+package com.automation.tests.vytrack.login;
+
+import com.automation.pages.LoginPage;
+import com.automation.tests.vytrack.TestBase;
+import org.testng.annotations.Test;
+
+public class NewLoginTests extends TestBase {
+    /**
+     * Login and verify that page title is a "Dashboard"
+     * 5 minutes
+     */
+    @Test
+    public void verifyPageTitle(){
+        LoginPage loginPage = new LoginPage();
+
+    }
+}
