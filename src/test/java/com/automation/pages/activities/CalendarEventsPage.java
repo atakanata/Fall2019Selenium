7ec089367c4d39ee650e484cef813c88bd34e6e9
cybertekschool <github@cybertekschool.com>
2020-03-29 12:45:35 -0400

    day 15 page object model

--- src/test/java/com/automation/pages/activities/CalendarEventsPage.java
+++ src/test/java/com/automation/pages/activities/CalendarEventsPage.java
@@ -12,6 +12,15 @@ public class CalendarEventsPage extends AbstractPageBase {
     @FindBy(css = "[title='Create Calendar event']")
     private WebElement createCalendarEvent;
 
+    @FindBy(className = "select2-chosen")
+    private WebElement owner;
+
+
+    public String getOwnerName(){
+        BrowserUtils.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.visibilityOf(owner));
+        return owner.getText().trim();
+    }
 
     public void clickToCreateCalendarEvent(){
         BrowserUtils.waitForPageToLoad(10);
