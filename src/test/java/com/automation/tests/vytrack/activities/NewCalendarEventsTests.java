1f7881cab83057f03b83f1bd34e43f64b63d34c6
cybertekschool <github@cybertekschool.com>
2020-03-29 13:24:55 -0400

    day 15 page object model

---
+++ src/test/java/com/automation/tests/vytrack/activities/NewCalendarEventsTests.java
@@ -0,0 +1,27 @@
+package com.automation.tests.vytrack.activities;
+
+import com.automation.pages.LoginPage;
+import com.automation.pages.activities.CalendarEventsPage;
+import com.automation.tests.vytrack.AbstractTestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NewCalendarEventsTests extends AbstractTestBase {
+    LoginPage loginPage = new LoginPage();
+    CalendarEventsPage calendarEventsPage = new CalendarEventsPage();
+
+    /**
+     * Test Case: Default options
+     * Login as sales manager
+     * Go to Activities --> Calendar Events
+     * Click on Create Calendar Event
+     * Default owner name should be current user/
+     **/
+    @Test
+    public void defaultOptionsTest() {
+        loginPage.login();
+        calendarEventsPage.navigateTo("Activities", "Calendar Events");
+        calendarEventsPage.clickToCreateCalendarEvent();
+        Assert.assertEquals(calendarEventsPage.getOwnerName(), calendarEventsPage.getCurrentUserName());
+    }
+}
