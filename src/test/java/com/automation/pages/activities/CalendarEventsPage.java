1ece9faafbbfc2bd0b8b23772f80853dbc4505b0
cybertekschool <github@cybertekschool.com>
2020-03-29 13:25:26 -0400

    day 15 page object model

--- src/test/java/com/automation/pages/activities/CalendarEventsPage.java
+++ src/test/java/com/automation/pages/activities/CalendarEventsPage.java
@@ -2,7 +2,6 @@ package com.automation.pages.activities;
 
 import com.automation.pages.AbstractPageBase;
 import com.automation.utilities.BrowserUtils;
-import com.sun.tools.javac.util.StringUtils;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
