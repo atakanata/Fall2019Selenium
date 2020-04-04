94923cccc7a3b7f15f94485c5c710d537f34d1c1
atakanata <atakanata@gmail.com>
2020-04-04 13:28:08 -0400

    4-4

--- src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -6,6 +6,7 @@ import com.automation.tests.vytrack.TestBase;
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.Driver;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class NewLoginTests extends AbstractTestBase {
@@ -30,5 +31,26 @@ public class NewLoginTests extends AbstractTestBase {
         Assert.assertEquals(loginPage.getWarningMessageText(), "Invalid user name or password.");
         //BrowserUtils.getScreenshot("loginPage");
         test.pass("Warning message was verified.");
+    }
+
+    @Test(dataProvider = "credentials")
+    public void loginWithDDT(String userName, String password) {
+        test = report.createTest("Verify page title");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(userName, password);
+        test.info("Login as " + userName);//log some steps
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
+        test.pass("Page title Dashboard was verified");
+    }
+    //Object[][] or Object[] or Iterator<Object[]>
+//Object[] - 1 column with a data
+//Object[][] 2+
+    @DataProvider
+    public Object[][] credentials() {
+        return new Object[][]{
+                {"storemanager85", "UserUser123"},
+                {"salesmanager110", "UserUser123"},
+                {"user16", "UserUser123"}
+        };
     }
 }
