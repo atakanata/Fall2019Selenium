283d12e66f08ee0310550bd806ec660268660da0
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-14 21:46:28 -0400

    write data into excel

--- src/test/java/com/automation/tests/vytrack/login/LoginTestsWithExcel.java
+++ src/test/java/com/automation/tests/vytrack/login/LoginTestsWithExcel.java
@@ -8,16 +8,19 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class LoginTestsWithExcel extends AbstractTestBase {
-    static int row = 1;
+
 
     @Test(dataProvider = "credentialsFromExcel")
     public void loginTestWithExcel2(String execute, String username, String password, String firstname, String lastname, String result) {
-
+        //determine excel file name
         String path = "VytrackTestUsers.xlsx";
+        //spreadsheet name
         String spreadSheet = "QA3-short";
-        ExcelUtil excelUtil = new ExcelUtil(path, spreadSheet);
+        //create object of excel utility class so we can write into the file
+        excelUtil = new ExcelUtil(path, spreadSheet);
 
         test = report.createTest("Login test for username :: " + username);
+        //if value of first column is y, test will be executed
         if (execute.equals("y")) {
 
             LoginPage loginPage = new LoginPage();
@@ -25,10 +28,13 @@ public class LoginTestsWithExcel extends AbstractTestBase {
             test.info("Login as " + username);//log some steps
             test.info(String.format("First name: %s, Last name: %s, Username: %s", firstname, lastname, username));
             test.pass("Successfully logged in as " + username);
+            //write into excel file that test passed
             excelUtil.setCellData("PASSED", "result", row++);
 
         } else if (execute.equals("n")) {
+
             test.skip("Test was skipped for user: " + username);
+            //write into excel that test was skipped, because first column has value "n"
             excelUtil.setCellData("SKIPPED", "result", row++);
             //to skip some test in testng
             throw new SkipException("Test was skipped for user: " + username);
